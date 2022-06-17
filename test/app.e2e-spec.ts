57fd271d6e53afc89bf2cfd25c345e5bf3b29fe4
IdrisovII <idrisovii@fsa.gov.ru>
2022-06-17 18:16:49 +0300

    Проверка работы е2е тестов

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -6,7 +6,7 @@ import { AppModule } from '../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -15,10 +15,16 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('GET /', () => {
+    it('should return "Hello World"', () => {
+      return request(app.getHttpServer())
+        .get('/')
+        .expect(200)
+        .expect('Hello World!');
+    });
   });
 });
